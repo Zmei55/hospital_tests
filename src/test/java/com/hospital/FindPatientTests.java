3ae9805b36a53aa188a7b4d0e541c12cfb380a5f
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-04-30 23:06:53 +0200

    FindPatientTests refactor

--- src/test/java/com/hospital/FindPatientTests.java
+++ src/test/java/com/hospital/FindPatientTests.java
@@ -1,6 +1,5 @@
 package com.hospital;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -10,59 +9,55 @@ public class FindPatientTests extends TestBase {
         // 2. логинемся
     @BeforeMethod
     public void ensurePrecondition() {
-        if (!isElementPresent(By.xpath("//h2[contains(.,'Autorisierung')]"))) {
-            driver.findElement(By.xpath("//button[contains(.,'Abmelden')]"));
+        if (!isLoginLinkPresent()) {
+            clickOnSignoutButton();
         } else {
-            type(By.xpath("//input[@placeholder='Login']"), "qwe");
-            type(By.xpath("//input[@placeholder='Password']"), "qwe123");
-            click(By.xpath("//button[contains(.,'Weiter')]"));
-            click(By.xpath("//input[@value='chirurgisch']"));
-            click(By.xpath("//button[contains(.,'Log In')]"));
-            assertTrueElement(By.xpath("//button[contains(.,'Abmelden')]"));
+            fillLoginForm("qwe", "qwe123", "//input[@value='chirurgisch']");
+            clickOnLoginButton();
+            isLoggedUser();
         }
     }
 
     @Test
     public void findPatientPositiveTest() {
         // клик: "Новый запрос"
-        click(By.xpath("//button[contains(.,'Erstellen Sie eine Bestellung')]"));
+        clickOnNewRequstButton();
 
         // проверка: находимся на странице "Новый запрос"
-        assertTrueElement(By.xpath("//h2[contains(.,'Neue Bestellung')]"));
+        isNewRequestPresent();
 
         // клик: "Выбрать пациента"
-        click(By.xpath("//button[contains(.,'Wählen Sie einen Patient aus')]"));
+        clickOnFindPatientButton();
 
         // проверка: находимся в модальном окне поиска пациента
-        assertTrueElement(By.xpath("//h3[contains(.,'Wählen Sie einen Patient aus')]"));
+        isModalFindPatientPresent();
 
         // ввод: данные пациента
-        type(By.xpath("//input[@name='name']"), "Emma Weber");
-//        type(By.xpath("//input[@name='birthDate']"), "26051968");
-//        type(By.xpath("//input[@name='cardNumber']"), "123456789");
+        fillFindPatient("Emma Weber");
 
         // клик: кнопка "Поиск"
-        click(By.xpath("//button[contains(.,'Finden')]"));
+        clickOnFindButton();
 
         // проверка: пациент нашёлся
-        assertTrueElement(By.className("css-16uxjix"));
+        isFoundPatientsPresent();
 
         // выбрать пациента (клик: "Выбрать")
-        click(By.xpath("//p[contains(.,'Wählen')]"));
+        clickOnSelectButton();
 
         // проверка: блок с информацией пациента появился
-        assertTrueElement(By.className("css-l2t4tz"));
+        isPatientPresent();
 
         // клик: кнопка "Доп.инфа" чтоб открылась
-        click(By.name("InfoButton"));
+        clickOnInfoButton();
 
         // проверка: блок с дополнительной информацией пациента появился
-        assertTrueElement(By.xpath("//h4[contains(.,'Wohnort')]"));
+        isSecondaryInfoPresent();
 
         // клик: кнопка "Доп.инфа" чтоб закрылась
-        click(By.name("InfoButton"));
+        clickOnInfoButton();
 
         // проверка: блок с дополнительной информацией пациента исчез
-        assertFalseElement(By.xpath("//h4[contains(.,'Wohnort')]"));
+        isNotSecondaryInfoPresent();
     }
+
 }
