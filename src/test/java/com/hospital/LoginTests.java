344fe760b3a1ff846c1106c378bedae0408ab49d
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-04-30 23:06:20 +0200

    LoginTests refactor

--- src/test/java/com/hospital/LoginTests.java
+++ src/test/java/com/hospital/LoginTests.java
@@ -8,105 +8,85 @@ public class LoginTests extends TestBase {
     // пользователь не зарегистрирован
     @BeforeMethod
     public void ensurePrecondition() {
-        if (!isElementPresent(By.xpath("//h2[contains(.,'Autorisierung')]"))) {
-            driver.findElement(By.xpath("//button[contains(.,'Abmelden')]"));
+        if (!isLoginLinkPresent()) {
+            clickOnSignoutButton();
         }
     }
 
     @Test(priority = 1)
     public void loginUserPositiveTest() {
         // проверка, наличие логин-формы
-        assertTrueElement(By.xpath("//h2[contains(.,'Autorisierung')]"));
+        isLoginFormPresent();
 
         // заполнение логин и пароль
-        type(By.xpath("//input[@placeholder='Login']"), "qwe");
-        type(By.xpath("//input[@placeholder='Password']"), "qwe123");
-
-        // клик на кнопку "далее"
-        click(By.xpath("//button[contains(.,'Weiter')]"));
-
-        // выбор стационара (клик по радио-баттон)
-        click(By.xpath("//input[@value='chirurgisch']"));
+        fillLoginForm("qwe", "qwe123", "//input[@value='chirurgisch']");
 
         // клик по кнопке "Логин"
-        click(By.xpath("//button[contains(.,'Log In')]"));
+        clickOnLoginButton();
 
         // проверка, залогинился ли пользователь
-        assertTrueElement(By.xpath("//button[contains(.,'Abmelden')]"));
+        isLoggedUser();
     }
 
     @Test(priority = 2)
     public void loginUserWithInvalidLoginNegativeTest() {
         // проверка, наличие логин-формы
-        assertTrueElement(By.xpath("//h2[contains(.,'Autorisierung')]"));
+        isLoginFormPresent();
 
         // заполнение логин и пароль
-        type(By.xpath("//input[@placeholder='Login']"), "asd");
-        type(By.xpath("//input[@placeholder='Password']"), "qwe123");
-
-        // клик на кнопку "далее"
-        click(By.xpath("//button[contains(.,'Weiter')]"));
-
-        // выбор стационара (клик по радио-баттон)
-        click(By.xpath("//input[@value='chirurgisch']"));
+        fillLoginForm("asd", "qwe123", "//input[@value='chirurgisch']");
 
         // клик по кнопке "Логин"
-        click(By.xpath("//button[contains(.,'Log In')]"));
+        clickOnLoginButton();
 
         // проверка, наличия ошибки
-        assertTrueElement(By.xpath("//p[contains(.,'Benutzerdaten wurden falsch eingegeben')]"));
+        isModalErrorPresent();
 
         // клик по кнопке Ок
-        click(By.xpath("//button[contains(.,'Ok')]"));
+        clickOnOkButton();
     }
 
     @Test(priority = 3)
     public void loginUserWithInvalidPasswordNegativeTest() {
         // проверка, наличие логин-формы
-        assertTrueElement(By.xpath("//h2[contains(.,'Autorisierung')]"));
+        isLoginFormPresent();
 
         // заполнение логин и пароль
-        type(By.xpath("//input[@placeholder='Login']"), "qwe");
-        type(By.xpath("//input[@placeholder='Password']"), "asd123");
-
-        // клик на кнопку "далее"
-        click(By.xpath("//button[contains(.,'Weiter')]"));
-
-        // выбор стационара (клик по радио-баттон)
-        click(By.xpath("//input[@value='chirurgisch']"));
+        fillLoginForm("qwe", "asd123", "//input[@value='chirurgisch']");
 
         // клик по кнопке "Логин"
-        click(By.xpath("//button[contains(.,'Log In')]"));
+        clickOnLoginButton();
 
         // проверка, наличия ошибки
-        assertTrueElement(By.xpath("//p[contains(.,'Benutzerdaten wurden falsch eingegeben')]"));
+        isModalErrorPresent();
 
         // клик по кнопке Ок
-        click(By.xpath("//button[contains(.,'Ok')]"));
+        clickOnOkButton();
     }
 
     @Test(priority = 4)
     public void loginUserWithInvalidStationNegativeTest() {
         // проверка, наличие логин-формы
-        assertTrueElement(By.xpath("//h2[contains(.,'Autorisierung')]"));
+        isLoginFormPresent();
 
         // заполнение логин и пароль
-        type(By.xpath("//input[@placeholder='Login']"), "qwe");
-        type(By.xpath("//input[@placeholder='Password']"), "qwe123");
-
-        // клик на кнопку "далее"
-        click(By.xpath("//button[contains(.,'Weiter')]"));
-
-        // выбор стационара (клик по радио-баттон)
-        click(By.xpath("//input[@value='neurologisch']"));
+        fillLoginForm("qwe", "qwe123", "//input[@value='neurologisch']");
 
         // клик по кнопке "Логин"
-        click(By.xpath("//button[contains(.,'Log In')]"));
+        clickOnLoginButton();
 
         // проверка, наличия ошибки
-        assertTrueElement(By.xpath("//p[contains(.,'Benutzerdaten wurden falsch eingegeben')]"));
+        isModalErrorPresent();
 
         // клик по кнопке Ок
+        clickOnOkButton();
+    }
+
+    private void clickOnOkButton() {
         click(By.xpath("//button[contains(.,'Ok')]"));
+    }
+
+    private void isModalErrorPresent() {
+        assertTrueElement(By.xpath("//p[contains(.,'Benutzerdaten wurden falsch eingegeben')]"));
     }
 }
