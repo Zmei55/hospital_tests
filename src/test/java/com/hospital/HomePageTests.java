0c489116ed3c9fa728ed7365f856180806de8380
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-04-30 23:08:13 +0200

    HomePageTests refactor

--- src/test/java/com/hospital/HomePageTests.java
+++ src/test/java/com/hospital/HomePageTests.java
@@ -2,7 +2,6 @@ package com.hospital;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomePageTests extends TestBase {
@@ -12,7 +11,7 @@ public class HomePageTests extends TestBase {
 //        driver.findElement(By.xpath("//button[contains(.,'Weiter')]")); // находим елемент: кнопка с текстом "Weiter"
 //        System.out.println(isHomeComponentPresent());
 //        System.out.println(isElementPresent(By.xpath("//button[contains(.,'Weiter')]")));
-        assertTrueElement(By.xpath("//button[contains(.,'Weiter')]"));
+        isLoginFormPresent();
     }
 
     public boolean isHomeComponentPresent(){
