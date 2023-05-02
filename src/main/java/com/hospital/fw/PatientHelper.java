7fc97e512a80bcf449abad6804909c0b8b85be82
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-05-02 12:33:00 +0200

    HomePageHelper, PatientHelper, UserHelper added

---
+++ src/main/java/com/hospital/fw/PatientHelper.java
@@ -0,0 +1,72 @@
+package com.hospital.fw;
+
+import com.hospital.model.Patient;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class PatientHelper extends BaseHelper {
+    public PatientHelper(WebDriver driver) {
+        super(driver);
+    }
+
+    public void fillFindPatient(Patient patient) {
+        type(By.xpath("//input[@name='name']"), patient.getName());
+    }
+
+    public void isNewRequestPresent() {
+        assertTrueElement(By.xpath("//h2[contains(.,'Neue Bestellung')]"));
+    }
+
+    public void isModalFindPatientPresent() {
+        assertTrueElement(By.xpath("//h3[contains(.,'Wählen Sie einen Patient aus')]"));
+    }
+
+    public void isFoundPatientsPresent() {
+        Assert.assertTrue(isElementsPresent(By.className("css-16uxjix")));
+    }
+
+    public void isNotSecondaryInfoPresent() {
+        assertFalseElement(By.xpath("//h4[contains(.,'Wohnort')]"));
+    }
+
+    public void isSecondaryInfoPresent() {
+        assertTrueElement(By.xpath("//h4[contains(.,'Wohnort')]"));
+    }
+
+    public void clickOnInfoButton() {
+        click(By.name("InfoButton"));
+    }
+
+    public void isPatientPresent() {
+        assertTrueElement(By.className("css-l2t4tz"));
+    }
+
+    public void clickOnSelectButton() {
+        click(By.xpath("//p[contains(.,'Wählen')]"));
+    }
+
+    public void clickOnNewRequstButton() {
+        click(By.xpath("//button[contains(.,'Erstellen Sie eine Bestellung')]"));
+    }
+
+    public void clickOnFindPatientButton() {
+        click(By.xpath("//button[contains(.,'Wählen Sie einen Patient aus')]"));
+    }
+
+    public void clickOnFindButton() {
+        click(By.xpath("//button[contains(.,'Finden')]"));
+    }
+
+    public void clickOnRemoveFoundPatientButton() {
+        click(By.name("ClearButton"));
+    }
+
+    public void isFindPatientButtonPresent() {
+        assertTrueElement(By.xpath("//button[@aria-label='find patient']"));
+    }
+
+    public void isPatientNotFound() {
+        assertTrueElement(By.className("css-1xpd0js"));
+    }
+}
---
+++ src/main/java/com/hospital/fw/UserHelper.java
@@ -0,0 +1,50 @@
+package com.hospital.fw;
+
+import com.hospital.model.User;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class UserHelper extends BaseHelper {
+    public UserHelper(WebDriver driver) {
+        super(driver);
+    }
+
+
+    public boolean isLoginFormPresent() {
+        return isElementPresent(By.xpath("//h2[contains(.,'Autorisierung')]")); // наличие формы логина
+    }
+
+
+    public void fillLoginForm(User user) {
+        type(By.xpath("//input[@placeholder='Login']"), user.getLogName()); // заполнить логин
+        type(By.xpath("//input[@placeholder='Password']"), user.getPassword()); // заполнить пароль
+
+        click(By.xpath("//button[contains(.,'Weiter')]")); // клик на кнопку "далее"
+
+        click(By.xpath(isStationName(user.getStation()))); // выбор стационара (клик по радио-кнопке)
+    }
+
+    public String isStationName(String locator) {
+        return "//input[@value="+"'"+locator+"'"+"]"; // локатор для стационара
+    }
+
+    public void clickOnSignoutButton() {
+        click(By.xpath("//button[contains(.,'Abmelden')]")); // клик кнопки "Выход"
+    }
+
+    public void clickOnLoginButton() {
+        click(By.xpath("//button[contains(.,'Log In')]"));
+    } // клик кнопки входа
+
+    public void isLoggedUser() {
+        assertTrueElement(By.xpath("//button[contains(.,'Abmelden')]"));
+    } // пользователь залогинен
+
+    public void clickOnOkButton() {
+        click(By.xpath("//button[contains(.,'Ok')]"));
+    }
+
+    public void isModalErrorPresent() {
+        assertTrueElement(By.xpath("//p[contains(.,'Benutzerdaten wurden falsch eingegeben')]"));
+    }
+}
